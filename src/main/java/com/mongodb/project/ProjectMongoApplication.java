81a7ca967cd6250c3a2e907d0a6eaabeac193b4b
Geovana <geovanamedeiro7@gmail.com>
2025-06-03 11:39:14 -0300

    first commit

---
+++ src/main/java/com/mongodb/project/ProjectMongoApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMongoApplication.class, args);
+	}
+
+}
