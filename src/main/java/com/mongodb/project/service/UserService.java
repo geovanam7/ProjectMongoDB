7edaf3c52c70a4d826dabc3a5dc8201a5c39d620
Geovana <geovanamedeiro7@gmail.com>
2025-06-10 15:14:56 -0300

    insert implemented

--- src/main/java/com/mongodb/project/service/UserService.java
+++ src/main/java/com/mongodb/project/service/UserService.java
@@ -1,6 +1,7 @@
 package com.mongodb.project.service;
 
 import com.mongodb.project.domain.User;
+import com.mongodb.project.dto.UserDTO;
 import com.mongodb.project.repository.UserRepository;
 import com.mongodb.project.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
 
 
     return user;
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO (UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
     }
  }
